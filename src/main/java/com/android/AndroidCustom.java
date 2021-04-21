8da3bf93dd5cb393d132256a425c1295a920baee
root <rohankar1999@gmail.com>
2021-04-21 15:18:16 +0800

    linode_2

--- src/main/java/com/android/AndroidCustom.java
+++ src/main/java/com/android/AndroidCustom.java
@@ -2,7 +2,16 @@ package com.android;
 
 public class AndroidCustom {
 
+    public void Custom(){
+        System.out.println("The program is working");
+
+    }
+
 	public static void main(String[] args) {
-		System.out.println("Hellow world");
+        System.out.println("Hellow world");
+        
+        AndroidCustom obj = new AndroidCustom();
+        obj.Custom();
 	}
 }
+
